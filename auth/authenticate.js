1556de22629d69f5386220b2e7b43f792f4601b9
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-08-02 14:02:42 -0700

    initial filestructure in place

---
+++ auth/authenticate.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+
+const jwtKey =
+  process.env.JWT_SECRET ||
+  'rumplestiltzken';
+
+// quickly see what this file exports
+module.exports = {
+  authenticate,
+  jwtKey
+};
+
+// implementation details
+function authenticate(req, res, next) {
+  const token = req.get('Authorization');
+
+  if (token) {
+    jwt.verify(token, jwtKey, (err, decoded) => {
+      if (err) return res.status(401).json(err);
+
+      req.decoded = decoded;
+
+      next();
+    });
+  } else {
+    return res.status(401).json({
+      error: 'No token provided, must be set on the Authorization Header',
+    });
+  }
+}
